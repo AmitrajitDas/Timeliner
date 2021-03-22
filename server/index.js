2271bf71adf382b42d9dd7f7e92a6cf1302042ab
Amitrajit31 <amitrajitdas31@gmail.com>
2021-03-22 12:44:02 +0530

    mongodb connection successful

--- server/index.js
+++ server/index.js
@@ -2,3 +2,21 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config()
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL = process.env.CONNECTION_URL;
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser : true, useUnifiedTopology : true   })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port : ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+mongoose.set('useFindAndModify', false);
