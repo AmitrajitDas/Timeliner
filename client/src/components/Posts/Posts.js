6912e184fdcc7a211f91590ae9e6bd80ef8ac110
Amitrajit31 <amitrajitdas31@gmail.com>
2021-03-22 15:33:35 +0530

    posts component structured and stryled

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import Post from './Post/Post';
+import useStyles from './styles';
+
 
 const Posts = () => {
+    const classes = useStyles();
     return (
         <>
         <h1>POSTS</h1>
