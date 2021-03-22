c1eae3dc656ff61dfc91e65e2903434832231c17
Amitrajit31 <amitrajitdas31@gmail.com>
2021-03-22 15:18:56 +0530

    app structured

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import { Container, AppBar, Grid, Grow, Typography } from '@material-ui/core';
+
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
+
+import memories from './images/memories.png';
 
 const App = () => {
     return(
-        <div>
-            <h1>App</h1>
-        </div>
+        <Container maxWidth="lg">
+            <AppBar position="static" color="inherit">
+                <Typography variant="h2" align="center">Memories</Typography>
+                <img src={memories} alt="memories" height="60" />
+            </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+                        <Grid item xs={12} sm={7}>
+                            <Posts />
+                        </Grid> 
+
+                        <Grid item xs={12} sm={4}>
+                            <Form />
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Grow>
+        </Container>
     ); 
 }
 
