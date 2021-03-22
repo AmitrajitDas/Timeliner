c6a56820872901e5b5caccbf0813f3acaa7a38cb
Amitrajit31 <amitrajitdas31@gmail.com>
2021-03-22 13:16:42 +0530

    post request added

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,9 +1,10 @@
 import express from 'express';
 
+import { getPosts, createPosts } from '../controllers/posts.js';
+
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('THIS WORKS!');
-});
+router.get('/', getPosts);
+router.post('/', createPosts);
 
 export default router;
