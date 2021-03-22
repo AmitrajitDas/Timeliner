932c3c93af8fac47116bf98bd5b37baf50c17537
Amitrajit31 <amitrajitdas31@gmail.com>
2021-03-22 12:54:24 +0530

    posts route added in app

--- server/index.js
+++ server/index.js
@@ -4,9 +4,13 @@ import cors from 'cors';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
-dotenv.config()
+import postRoutes from './routes/posts.js';
+
+dotenv.config();
 
 const app = express();
+
+app.use('/posts', postRoutes);
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
