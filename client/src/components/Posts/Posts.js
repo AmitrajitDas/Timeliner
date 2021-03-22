15fc1468d377b1ddf9eec06430205b95d332910a
Amitrajit31 <amitrajitdas31@gmail.com>
2021-03-22 15:18:29 +0530

    form and posts component

---
+++ client/src/components/Posts/Posts.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Post from './Post/Post';
+
+const Posts = () => {
+    return (
+        <>
+        <h1>POSTS</h1>
+        <Post />
+        <Post />
+        </>
+    );
+}
+
+export default Posts;
