74e073ce17c0928039f96d224140fbef92def266
Amitrajit31 <amitrajitdas31@gmail.com>
2021-03-22 15:34:39 +0530

    added form and posts component in app component and refactored

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,19 @@ import { Container, AppBar, Grid, Grow, Typography } from '@material-ui/core';
 
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
+import useStyles from './styles';
 
 import memories from './images/memories.png';
 
 const App = () => {
+
+    const classes = useStyles();
+
     return(
         <Container maxWidth="lg">
-            <AppBar position="static" color="inherit">
-                <Typography variant="h2" align="center">Memories</Typography>
-                <img src={memories} alt="memories" height="60" />
+            <AppBar className={classes.appBar} position="static" color="inherit">
+                <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
+                <img className={classes.image} src={memories} alt="memories" height="60" />
             </AppBar>
             <Grow in>
                 <Container>
