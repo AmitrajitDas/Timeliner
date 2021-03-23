7d6c2deb07f985601225d0b92a4b0fd0cf965f51
Amitrajit31 <amitrajitdas31@gmail.com>
2021-03-23 12:32:48 +0530

    implementing useSelector hook to handle post reducer

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+
 import Post from './Post/Post';
 import useStyles from './styles';
 
 
 const Posts = () => {
+    
+    const posts = useSelector((state) => state.posts);
     const classes = useStyles();
+
+    console.log(posts);
+
     return (
         <>
         <h1>POSTS</h1>
