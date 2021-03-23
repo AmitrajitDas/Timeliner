1780f253184f6ece47d48f345c4fb1388994c0f3
Amitrajit31 <amitrajitdas31@gmail.com>
2021-03-23 12:31:22 +0530

    replaced posts with action payload

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -2,7 +2,7 @@
 const reducer =  (posts = [], action) => {
     switch (action.type) {
         case 'FETCH_ALL' :
-            return posts;
+            return action.payload;
         case 'CREATE' :
             return posts;
         default:
