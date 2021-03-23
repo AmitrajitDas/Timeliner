67e3d4722a6257bb9976f2800498f077f1f7e170
Amitrajit31 <amitrajitdas31@gmail.com>
2021-03-23 12:31:50 +0530

    resolved cors eerror

--- server/index.js
+++ server/index.js
@@ -10,11 +10,13 @@ dotenv.config();
 
 const app = express();
 
-app.use('/posts', postRoutes);
+
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+app.use('/posts', postRoutes);
 
 const CONNECTION_URL = process.env.CONNECTION_URL;
 const PORT = process.env.PORT || 5000;
