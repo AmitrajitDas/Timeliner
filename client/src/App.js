cf1db54c0c4d2ac57f94d5e735fbc8929446b5c0
Amitrajit31 <amitrajitdas31@gmail.com>
2021-04-05 09:40:33 +0530

    added mobile responsiveness

--- client/src/App.js
+++ client/src/App.js
@@ -27,7 +27,7 @@ const App = () => {
             </AppBar>
             <Grow in>
                 <Container>
-                    <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+                    <Grid container className={classes.mainContainer} justify="space-between" alignItems="stretch" spacing={3}>
                         <Grid item xs={12} sm={7}>
                             <Posts setCurrentId={setCurrentId}  />
                         </Grid> 
