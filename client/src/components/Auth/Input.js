d11e2bf05ac2d1c6c350a275890783c1c37a633c
Amitrajit31 <amitrajitdas31@gmail.com>
2021-04-07 10:57:08 +0530

    auth form input component

---
+++ client/src/components/Auth/Input.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { TextField, Grid, InputAdornment, IconButton } from '@material-ui/core';
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
+
+const Input = ({ name, handleChange, label, half, autofocus, type,handleShowPassword }) => {
+    return (
+        <Grid item xs={12} sm={half ? 6 : 12}>
+            <TextField
+                name={name}
+                onChange={handleChange}
+                variant="outlined"
+                required
+                fullWidth
+                label={label}
+                autofocus={autofocus}
+                type={type}
+                InputProps={ name === 'password' && {
+                    endAdornment : (
+                        <InputAdornment position="end">
+                            <IconButton onClick={handleShowPassword}>
+                                {type === 'password' ? <Visibility /> : <VisibilityOff />}
+                            </IconButton>
+                        </InputAdornment>
+                    ),
+                }}
+             />
+        </Grid>
+    );
+}
+
+export default Input;
