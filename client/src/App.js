1f4a01a64b539256e8e7f3962df15d4064a25eb7
Amitrajit31 <amitrajitdas31@gmail.com>
2021-04-07 10:57:27 +0530

    refactored

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Navbar from './components/Navbar/Navbar';
 import  Home  from './components/Home/Home';
+import Auth from './components/Auth/Auth';
 
 
 const App = () => {
@@ -18,7 +19,6 @@ const App = () => {
                     <Route path="/" exact component={Home} />
                     <Route path="/auth" exact component={Auth} />
                 </Switch>
-                <Home />
             </Container>
         </BrowserRouter>
         
