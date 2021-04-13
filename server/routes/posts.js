86f9811fd23f8d3c208d6a602986fceb230016d3
Amitrajit31 <amitrajitdas31@gmail.com>
2021-04-13 14:39:50 +0530

    added auth middleware in postRoutes

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,13 +1,14 @@
 import express from 'express';
 
 import { getPost, createPost, updatePost, deletePost, likePost } from '../controllers/posts.js';
+import auth from '../middleware/auth.js';
 
 const router = express.Router();
 
 router.get('/', getPost);
-router.post('/', createPost);
-router.patch('/:id', updatePost);
-router.delete('/:id', deletePost);
-router.patch('/:id/likePost', likePost);
+router.post('/', auth, createPost);
+router.patch('/:id', auth, updatePost);
+router.delete('/:id', auth, deletePost);
+router.patch('/:id/likePost', auth, likePost);
 
 export default router;
